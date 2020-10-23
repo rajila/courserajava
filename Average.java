0cf55ce83b6e8a8e487191f7130a3621f31c84f7
Ronald <rdajila@gmail.com>
2020-10-23 17:05:43 +0200

    File average

---
+++ Average.java
@@ -0,0 +1,22 @@
+/**
+    Compilar (W10):
+        javac -cp "algs4.jar" Average.java
+    Ejecutar (W10):
+        java -cp ".;algs4.jar" Average
+*/
+
+import edu.princeton.cs.algs4.*;
+
+public class Average {
+    public static void main (String[] args) {
+        double _suma = 0.0;
+        int _contador = 0;
+		StdOut.printf(" ++++ Excersise One ++++\n");
+        while(!StdIn.isEmpty()) { // ctrl-z para salir del lazo
+            _suma += StdIn.readDouble();
+            _contador++;
+        }
+        double _avg = _suma / _contador;
+        StdOut.printf("Average is %.5f\n", _avg);
+    }
+}

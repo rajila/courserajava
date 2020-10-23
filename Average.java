5bd68cc7227bd9c544f1a1e8bc843ed1a011c8bb
Ronald <rdajila@gmail.com>
2020-10-23 19:13:23 +0200

    Changes on references

--- Average.java
+++ Average.java
@@ -3,6 +3,9 @@
         javac -cp "algs4.jar" Average.java
     Ejecutar (W10):
         java -cp ".;algs4.jar" Average
+
+    Link of reference:
+    https://algs4.cs.princeton.edu/code/
 */
 
 import edu.princeton.cs.algs4.*;
@@ -11,7 +14,7 @@ public class Average {
     public static void main (String[] args) {
         double _suma = 0.0;
         int _contador = 0;
-		StdOut.printf(" ++++ Excersise One ++++\n");
+	    StdOut.printf(" ++++ Excersise One ++++\n");
         while(!StdIn.isEmpty()) { // ctrl-z para salir del lazo
             _suma += StdIn.readDouble();
             _contador++;
